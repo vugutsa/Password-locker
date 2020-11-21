cd8fb329189f5b8c4aea7ea16211558b9d49991d
vugutsa <vugutsamercy84@gmail.com>
2020-11-21 22:04:00 +0300

    add the init method to the user.py

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+  """
+  class that generate new instance of a user
+  """
+  
+def __init__(self,user_name,password):
+   """
+   __init__ method that helps us define the properties of our objects.
+   """
+   
+pass 
+      
