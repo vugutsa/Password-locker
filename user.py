f0a1aeabffd967365de3f81f13fbb261c8e0fca7
vugutsa <vugutsamercy84@gmail.com>
2020-11-22 14:24:26 +0300

    add the method of saving multiple users

--- user.py
+++ user.py
@@ -11,4 +11,11 @@ class User:
         self.user_name = user_name
         self.password = password
 
+        
+    def save_user(self):
+    
+        '''
+        save_user method saves user objects into user_list
+        '''
 
+        User.user_list.append(self) 
--- user_test.py
+++ user_test.py
@@ -14,8 +14,6 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("vugutsa","5657") # create user object
-
-
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
@@ -23,8 +21,32 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(self.new_user.user_name,"vugutsa")
         self.assertEqual(self.new_user.password,"5657")
-       
+        
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+        the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+  # setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
 
 
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Bella","6579") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+        
 if __name__ == '__main__':
     unittest.main()
